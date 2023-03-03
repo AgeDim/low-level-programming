597cf29932878c4d71e19ec592825950d9f66aad
AgeDim <proga778@gmail,com>
2023-03-03 11:59:42 +0300

    test for graphics done!

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 
 #include "../test/test.h"
+#include "../test/info.h"
 
 using namespace std;
 
 int main() {
 
     test test;
+    info info;
     //main test
     test.all_tests();
+    cout << endl;
+
+    info.insert_time();
+    cout << endl;
+    info.select_time();
+    cout << endl;
+    info.update_time();
+    cout << endl;
+    info.delete_time();
+    cout << endl;
+    info.empty_load_test();
+    cout << endl;
+    info.big_load_test();
     cout << endl;
 
     return 0;
